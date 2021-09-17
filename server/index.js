e94c30c77a56a8bfc908d7fbc7832616a679ea2e
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-09-17 08:13:19 +0100

    Feat: Add API endpoint Gets weather from latitude and lognitude provided Uses openweathermap API

--- server/index.js
+++ server/index.js
@@ -5,6 +5,22 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get("/api/weather/:latitude/:longitude", (req, res) => {
+    var latitude = req.params.latitude;
+    var longitude = req.params.longitude;
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${process.env.API_KEY}`;
+    fetch(apiUrl)
+        .then(res => res.json())
+        .then(data => {
+            res.send({ data });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+});
+
+
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
