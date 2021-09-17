86a86c1503371391f3c3aec2ef32b68c70a0badc
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-09-17 09:48:07 +0100

    Chore: Require node-fetch in index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const express = require("express");
+const express = require('express');
+const fetch = require('node-fetch');
 
 const PORT = process.env.PORT || 3001;
 
